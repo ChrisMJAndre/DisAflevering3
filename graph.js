d7cf30ed6f4e9811ba6c22056f3f0db5829b47ab
ChrisMJAndre <chan19af@student.cbs.dk>
2020-10-08 12:26:37 +0200

    changes in router

--- graph.js
+++ graph.js
@@ -12,14 +12,11 @@ g.addEdge(new jsgraphs.Edge(2, 3, 2));
 // 3. Create a function that takes a graph, a from node and a to node,
 //    and print the shortest path between the two nodes.
 const shortestPath = (graph, to, from) => {
-  let sp = [];
   // write the functionality here. Remember to look at the documentation!
   var dijkstra = new jsgraphs.Dijkstra(graph, from);
   if (dijkstra.hasPathTo(to)) {
     return dijkstra.pathTo(to).map((edge) => edge.to());
   } else return console.log("path does not exist!");
-  // return the shortest path as an array
-  return console.log(sp);
 };
 console.log(shortestPath(g, 3, 0));
 // (4.) Modify above function to handle it, if a path doesn't exist.
