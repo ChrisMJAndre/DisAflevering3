c19a030396996573038462e48256e177ef51938c
Christopher André <55132343+ChrisMJAndre@users.noreply.github.com>
2020-10-22 14:03:58 +0200

    formatting

--- v3/multipleRouters.js
+++ v3/multipleRouters.js
@@ -1,86 +1,85 @@
 let data = require("../data.json");
 const Router = require("./routerV3");
 const Packet = require("./packet");
-const prompt = require('prompt'); 
-const jsgraphs = require('js-graph-algorithms');
+const prompt = require("prompt");
+const jsgraphs = require("js-graph-algorithms");
 
 let routers = [];
 
 const multipleRouters = () => {
-    /**
-     * 1. Iterate through the data and create the routers from it 
-     * as well as add it to our array.
-     */
-   
-    // your code here
-    data.routers.forEach(router => {
-        // initialize router
-        let r = new Router(router.router, router.connections);
-        // add to array
-        routers.push(r)
-    })
+  /**
+   * 1. Iterate through the data and create the routers from it
+   * as well as add it to our array.
+   */
 
-    /**
-     * 2. build a weighted directional graph and adds the edges 
-     * between the nodes through the data.json file
-     */
-    
-    // your code here
-    // initliaze graph below
-    var g = new jsgraphs.WeightedDiGraph(4);
-    // add edges dynamically.
-    // Hint: You need a nested loop.
-    // The outer loop must iterate the routers, and the inner loop the connections.
+  // your code here
+  data.routers.forEach((router) => {
+    // initialize router
+    let r = new Router(router.router, router.connections);
+    // add to array
+    routers.push(r);
+  });
 
-    for (let i = 0; i < data.routers.length; i++) {
+  /**
+   * 2. build a weighted directional graph and adds the edges
+   * between the nodes through the data.json file
+   */
+
+  // your code here
+  // initliaze graph below
+  var g = new jsgraphs.WeightedDiGraph(4);
+  // add edges dynamically.
+  // Hint: You need a nested loop.
+  // The outer loop must iterate the routers, and the inner loop the connections.
+
+  for (let i = 0; i < data.routers.length; i++) {
     let r = data.routers[i];
     for (let x = 0; x < r.connections.length; x++) {
-        let c = r.connections[x];
-        g.addEdge(new jsgraphs.Edge(r.router, c.to, c.cost));
+      let c = r.connections[x];
+      g.addEdge(new jsgraphs.Edge(r.router, c.to, c.cost));
     }
+  }
+
+  /**
+   * 3. create a new packet.
+   * create a packet with a name, a source, a destination and a ttl.
+   * the source should be 0, destination 3 and ttl > 3.
+   * the name can be whatever you'd like.
+   */
+
+  let demoPacket = new Packet("Chris", 0, 3, 10);
+  // Add the shortest path to the packet.
+  demoPacket.shortestPath = getShortestPath(
+    g,
+    demoPacket.source,
+    demoPacket.destination
+  );
+
+  /**
+   * Prompt is a package to prompt the user though the terminal.
+   * Can be found here: https://github.com/flatiron/prompt#readme
+   */
+  prompt.start();
+  console.log("demo packet initialized. Send packet? (y/n)");
+  prompt.get(["sendPacket"], function (err, res) {
+    if (res.sendPacket == "y") {
+      demoPacket.forwardPacket(demoPacket.source);
+    } else {
+      console.log("Bye!");
+      process.exit(1);
     }
-    
-    /**
-     * 3. create a new packet. 
-     * create a packet with a name, a source, a destination and a ttl.
-     * the source should be 0, destination 3 and ttl > 3. 
-     * the name can be whatever you'd like.
-     */
-
-    let demoPacket = new Packet("Chris", 0, 3, 10);
-    // Add the shortest path to the packet.
-    demoPacket.shortestPath = getShortestPath(
-        g, 
-        demoPacket.source, 
-        demoPacket.destination
-        );
-
-    /**
-     * Prompt is a package to prompt the user though the terminal.
-     * Can be found here: https://github.com/flatiron/prompt#readme
-     */
-    prompt.start();
-    console.log("demo packet initialized. Send packet? (y/n)")
-    prompt.get(["sendPacket"], function(err, res) {
-        if(res.sendPacket == "y") {
-            demoPacket.forwardPacket(demoPacket.source);
-        }
-        else {
-            console.log("Bye!")
-            process.exit(1);
-        }
-    })
-}
+  });
+};
 
 /**
  * This methods gets the router names / indexes on the shortest path.
  */
 const getShortestPath = (graph, from, to) => {
-    var dijkstra = new jsgraphs.Dijkstra(graph, from);
-    if (dijkstra.hasPathTo(to)) {
-      return dijkstra.pathTo(to).map((edge) => edge.to());
-    } else return console.log("path does not exist!");
-    // return the shortest path as an array
-  };
+  var dijkstra = new jsgraphs.Dijkstra(graph, from);
+  if (dijkstra.hasPathTo(to)) {
+    return dijkstra.pathTo(to).map((edge) => edge.to());
+  } else return console.log("path does not exist!");
+  // return the shortest path as an array
+};
 
 multipleRouters();
--- v3/routerV3.js
+++ v3/routerV3.js
@@ -1,112 +1,112 @@
-const http = require('http');
-const seaport = require('seaport');
-const ports = seaport.connect('localhost', 9090);
-const fetch = require('node-fetch');
-const Packet = require('./packet');
-const { setFlagsFromString } = require('v8');
+const http = require("http");
+const seaport = require("seaport");
+const ports = seaport.connect("localhost", 9090);
+const fetch = require("node-fetch");
+const Packet = require("./packet");
+const { setFlagsFromString } = require("v8");
 
-class Router{
-    constructor(name, connections) {
-        this.name = name;
-        this.connections = connections;
-        var self = this; // trick to access router from inner functions
-        this.server = http.createServer(function(req, res) {
-            let data = []
-            // If the request contains any data, this listener is activated.
-            req.on('data', chunk => {
-                data.push(chunk)
-            })
+class Router {
+  constructor(name, connections) {
+    this.name = name;
+    this.connections = connections;
+    var self = this; // trick to access router from inner functions
+    this.server = http.createServer(function (req, res) {
+      let data = [];
+      // If the request contains any data, this listener is activated.
+      req.on("data", (chunk) => {
+        data.push(chunk);
+      });
 
-            // listener when reading data stream is done. 
-            req.on('end', () => {
-                if(data.length > 0) {
-                   
-                    //1. decipher data. hint: JSON.parse()
-                    data = JSON.parse(data);
-                    //2. reconstructing packet from data
-                    let packet = new Packet(
-                        data.id,
-                        data.source,
-                        data.destination,
-                        data.ttl,
-                        data.routingHistory,
-                        data.shortestPath
-                    );
-                    
-                    console.log(
-                        "\n" +
-                        "Packet " + 
-                        packet.id + 
-                        " received at router " + 
-                        self.name);
-                    
-                    if(packet.destination == self.name) {
-                        // 3. What to do if packet has reached destination? 
-                        // We should end.
-                        console.log("Packet " + packet.id + " reached its destination at a cost of " + packet.getTotalCost());
-                        res.end(JSON.stringify(data));
-                        return;
-                    } 
-                    // 4. Get which router to forwardTo. 
-                    // Hint: there's a method in packet, that gets the next router.
-                    // Hint: should be an int.
-                    let forwardTo = packet.popShortestPath();
-                    // get the connection object (routeTo). 
-                    // consists of a "to" and a "cost".
-                    let routeTo = self.getRouteTo(forwardTo);
-                    // 5. decrement the packets ttl.
-                    packet.ttl = packet.ttl - 1;
-                    // 6. Add an extra field to routeTo named ttl with same value as the packet's ttl.
-                    routeTo.ttl = packet.ttl;
-                    // remember the object notation of objects in javascript.
-                           
-                    // 7. Finish the if statement.
-                    if(packet.ttl == 0) {
-                        res.end(JSON.stringify({msg:"packed dropped due to ttl"}))
-                        return;
-                    }
-                    console.log(
-                        "Total Cost: " +
-                        packet.getTotalCost() + 
-                        "\n" + 
-                        "Forwarding to router "+
-                        forwardTo +
-                        " and inccuring a cost of " + 
-                        routeTo.cost                        
-                        );
-                    
-                    // 8. Add the routeTo to the packet's history.
-                    // hint: Look at the packet methods.
-                    packet.addRouteToHistory(routeTo);    
+      // listener when reading data stream is done.
+      req.on("end", () => {
+        if (data.length > 0) {
+          //1. decipher data. hint: JSON.parse()
+          data = JSON.parse(data);
+          //2. reconstructing packet from data
+          let packet = new Packet(
+            data.id,
+            data.source,
+            data.destination,
+            data.ttl,
+            data.routingHistory,
+            data.shortestPath
+          );
 
-                    // 9. forward the packet to the forwardTo variable.
-                    // again look at the packet's methods.
-                    packet.forwardPacket(forwardTo);
-                } else  {
-                    res.end("No data received");
-                }
-            });
-            
-        });
-        this.port = ports.register("router" + this.name);
-        this.server.listen(this.port, function() {
-            // console.log("Router " + name + " is listening on port " + this.address().port);
-        });
-    }
+          console.log(
+            "\n" + "Packet " + packet.id + " received at router " + self.name
+          );
 
-    getRouteTo = (destination) => {
-        let found;
-        this.connections.forEach((c) => {
-            if(c.to === destination) {
-                found = c;
-            }
-        })
-        return found;
-    }
+          if (packet.destination == self.name) {
+            // 3. What to do if packet has reached destination?
+            // We should end.
+            console.log(
+              "Packet " +
+                packet.id +
+                " reached its destination at a cost of " +
+                packet.getTotalCost()
+            );
+            res.end(JSON.stringify(data));
+            return;
+          }
+          // 4. Get which router to forwardTo.
+          // Hint: there's a method in packet, that gets the next router.
+          // Hint: should be an int.
+          let forwardTo = packet.popShortestPath();
+          // get the connection object (routeTo).
+          // consists of a "to" and a "cost".
+          let routeTo = self.getRouteTo(forwardTo);
+          // 5. decrement the packets ttl.
+          packet.ttl = packet.ttl - 1;
+          // 6. Add an extra field to routeTo named ttl with same value as the packet's ttl.
+          routeTo.ttl = packet.ttl;
+          // remember the object notation of objects in javascript.
 
-    updateConnections(connections) {
-        this.connections = this.connections;
-    }
+          // 7. Finish the if statement.
+          if (packet.ttl == 0) {
+            res.end(JSON.stringify({ msg: "packed dropped due to ttl" }));
+            return;
+          }
+          console.log(
+            "Total Cost: " +
+              packet.getTotalCost() +
+              "\n" +
+              "Forwarding to router " +
+              forwardTo +
+              " and inccuring a cost of " +
+              routeTo.cost
+          );
+
+          // 8. Add the routeTo to the packet's history.
+          // hint: Look at the packet methods.
+          packet.addRouteToHistory(routeTo);
+
+          // 9. forward the packet to the forwardTo variable.
+          // again look at the packet's methods.
+          packet.forwardPacket(forwardTo);
+        } else {
+          res.end("No data received");
+        }
+      });
+    });
+    this.port = ports.register("router" + this.name);
+    this.server.listen(this.port, function () {
+      // console.log("Router " + name + " is listening on port " + this.address().port);
+    });
+  }
+
+  getRouteTo = (destination) => {
+    let found;
+    this.connections.forEach((c) => {
+      if (c.to === destination) {
+        found = c;
+      }
+    });
+    return found;
+  };
+
+  updateConnections(connections) {
+    this.connections = this.connections;
+  }
 }
 
 module.exports = Router;
